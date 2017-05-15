7809467218d8cf8b440b7c1d9d25f5968308a8a2
linhai <linhai107@gmail.com>
2017-05-15 17:40:53 +0800

    implement carts index function

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,14 @@ Rails.application.routes.draw do
     resources :products
 
   end
-  
+
   resources :products do
     member do
      post :add_to_cart
     end
   end
+
+  resources :carts
 
 
 end
