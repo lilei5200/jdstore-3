b53946c655fdbb44cfcb9f755458c286619628f2
linhai <linhai107@gmail.com>
2017-05-20 21:31:29 +0800

    add notify_cancel/ship function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -43,6 +43,8 @@ class OrdersController < ApplicationController
     redirect_to order_path(@order.token), notice: "使用支付宝成功完成付款"
   end
 
+
+
   def pay_with_wechat
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("wechat")
