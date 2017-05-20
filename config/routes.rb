7120a8a0ed967cc4c13d81c324ab4d36c51e8bdc
linhai <linhai107@gmail.com>
2017-05-20 21:23:34 +0800

    add apply_cancel function

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
         post :ship
         post :shipped
         post :return
-        
+
       end
     end
 
@@ -38,6 +38,7 @@ Rails.application.routes.draw do
       member do
         post :pay_with_alipay
         post :pay_with_wechat
+        post :apply_to_cancel
       end
     end
 
